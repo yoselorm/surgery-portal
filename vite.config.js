f603a5a7a0a3b0ecfefdbfbf308f6939c0533ed6
Selorm Ayornu <95534257+yoselorm@users.noreply.github.com>
2025-12-18 17:43:00 +0000

    fix: vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/'
+   base: '/',
   // server: {
   //   host: '127.0.0.1', // Force 127.0.0.1
   //   port: 5173,
